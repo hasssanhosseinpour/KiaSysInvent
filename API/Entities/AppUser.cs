34012a7f949b1c202f675811b48c19c3be525058
Farideh Lamee Hassan Kiadeh <f_lamee2002@yahoo.com>
2023-09-01 22:26:00 -0700

    First Commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }    
+        public DateTime Created { get; set; } 
+        public DateTime Updated { get; set; }     
+    }
+}
